546535a2d6cef2ba1684208a52307d35ddb4dafc
teddy <teddywambua5456@gmail.com>
2023-05-29 22:34:39 +0300

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    validate_triangle
+  end
+
+  def kind
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    validate_positive_lengths
+    validate_triangle_inequality
+  end
+
+  def validate_positive_lengths
+    if side1 <= 0 || side2 <= 0 || side3 <= 0
+      raise TriangleError, "Triangle sides must have lengths greater than 0."
+    end
+  end
+
+  def validate_triangle_inequality
+    if side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2
+      raise TriangleError, "Triangle violates the triangle inequality principle."
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
